c779a04d5b213c8a610ffae745c53e90e251db41
AdamZeraoui <adam.zeroaui@hotmail.fr>
2024-02-27 14:21:18 +0100

    End

--- alert.js
+++ alert.js
@@ -1,14 +1,19 @@
-
-
-
-
 const cube = document.querySelector('#cube');
-var elem1 = document.getElementById(cube);
-var style = window.getComputedStyle(elem1);
+let style = window.getComputedStyle(cube);
+
+var id = cube.getAttribute( 'id' );
+
+function getInfoCube(){
+
+    const idName = "Class = " + id +" \n";
+    const backgroundColor = " - Background color = " + style.backgroundColor+ "\n";
+    const color = " - Color = " + style.color + "\n"
+    const height = " - Height = "+ style.height +"\n";
+    const width = " - Width = " + style.width +"\n";
+    const displayFlex  = " - Display = " + style.display + "\n";
+    const fontFamilyFontSize = " - Font Family = " + style.fontFamily +" ( "+ style.fontSize+" )";
+    alert (idName + backgroundColor + color + height + width + displayFlex + fontFamilyFontSize)
+}
 cube.addEventListener('click', function(){
-    window.alert(style);
+    getInfoCube()
 })
-
-
-
-
